2f0ca84eb79ec88dde0d86ab6cbed66038db30ab
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2026-03-02 02:05:02 +0200

    add quotes screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qoutes/features/random_qoutes/presentation/screens/quotes_screen.dart';
 
 class QoutesApp extends StatelessWidget {
   const QoutesApp({super.key});
@@ -7,11 +8,12 @@ class QoutesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+   debugShowCheckedModeBanner: false,
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: QuotesScreen(),
     
     );
   }

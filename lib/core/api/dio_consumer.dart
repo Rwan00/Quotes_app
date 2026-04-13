658a137a2da3549ca163115e3268005e9ebc14e0
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2026-04-13 17:58:25 +0200

    add app interceptor

--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
+import 'package:flutter/foundation.dart';
 import 'package:qoutes/core/api/api_consumer.dart';
+import 'package:qoutes/core/api/app_interceptors.dart';
 import 'package:qoutes/core/api/endpoints.dart';
 import 'package:qoutes/core/api/status_code.dart';
+import 'package:qoutes/injection_container.dart' as di;
 
 class DioConsumer implements ApiConsumer {
   final Dio client;
@@ -23,6 +26,10 @@ class DioConsumer implements ApiConsumer {
       ..validateStatus = (status) {
         return status! < StatusCode.internalServerError;
       };
+    client.interceptors.add(di.sl<AppInterceptors>());
+    if(kDebugMode){
+      client.interceptors.add(di.sl<LogInterceptor>());
+    }
   }
   @override
   Future get(String path, {Map<String, dynamic>? queryParameters}) {
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:qoutes/core/api/app_interceptors.dart';
 import 'package:qoutes/core/network/network_info.dart';
 import 'package:qoutes/features/random_qoutes/data/datasource/quote_local_datasource.dart';
 import 'package:qoutes/features/random_qoutes/data/datasource/quote_remote_datasource.dart';
@@ -48,8 +50,17 @@ Future<void> init() async {
 
   //! External
   final sharedPreference = await SharedPreferences.getInstance();
-  final connectionChecker =  InternetConnectionChecker.createInstance();
+  final connectionChecker = InternetConnectionChecker.createInstance();
   sl.registerLazySingleton<SharedPreferences>(() => sharedPreference);
   sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => AppInterceptors());
+  sl.registerLazySingleton(() => LogInterceptor(
+    request: true,
+    requestBody: true,
+    requestHeader: true,
+    responseBody: true,
+    responseHeader: true,
+    error: true,
+  ));
   sl.registerLazySingleton(() => connectionChecker);
 }
